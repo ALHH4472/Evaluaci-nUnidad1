bf34a1fa3aef8302b1d52b14989a3a1b579e0ec3
Licht <ahenriquez.4472@unimar.edu.ve>
2022-02-04 20:53:35 -0400

    Alli va el dos

---
+++ src/main/java/Ejecicio2.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+
+
+public class Ejecicio2 {
+    public static void main(String[] args){
+      Scanner entrada=new Scanner(System.in);  
+      
+      System.out.println("Dime el primer parametro de la matriz");
+     int i =entrada.nextInt();
+     System.out.println("Dime el segundo parametro de la matriz");
+     int j =entrada.nextInt();      
+    
+    
+    int [][] M= new int [i][j];
+    
+    for(int x=0;x< M.length;x++){
+        for(int y=0;y<M.length;y++){
+            M[x][y]=(23*(int) Math.pow(x, 4))+(20*(int) Math.pow(y, 3)-3);
+        }
+    }
+    
+         for(int r=0;r<M.length;r++){
+          System.out.println();
+          for(int x=0;x<M.length;x++){
+              System.out.print(M[r][x]);
+              System.out.print(" ");
+    }
+    
+    
+    
+    
+    
+}
+    }
+}
+    
